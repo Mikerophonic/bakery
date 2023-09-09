9a1a69f086e858a0e3e20d934d804663c634ed5d
Mikerophonic <micgcon@gmail.com>
2023-09-09 12:36:43 -0700

    add UI

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -9,7 +9,18 @@ namespace AnagramChecker
     {
         static void Main()
         {
-       
+            Console.WriteLine("Welcome to the Bakery!");
+            Console.WriteLine("Bread is $5 a loaf. Buy two, get one free!");
+            Console.WriteLine("How many loaves of bread would you like?");
+            int breadAmount = Convert.ToInt32(Console.ReadLine());
+
+            Bread newBread = new Bread(breadAmount);
+            int breadPrice = newBread.CalculateBreadPrice(newBread.Amount);
+            
+            Console.WriteLine($"The total cost of your bread order is: ${breadPrice}");
+
+
+
             
         }
     }
